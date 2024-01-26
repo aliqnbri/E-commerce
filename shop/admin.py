534fc88eba2e848684061662d4ec3d103f6e4447
aliqnbri <aliqnbri1998@gmail.com>
2024-01-27 00:30:28 +0330

    KEEP RECORD DAY

--- shop/admin.py
+++ shop/admin.py
@@ -1,67 +1,126 @@
 from django.contrib import admin
+from .models import Category, Author , Book
 
 # Register your models here.
-from .models import Category, Product
 
 
-# @admin.register(Product)
-class ProductsInline(admin.TabularInline):
-    model = Product
-    extra = 3  # Add three more products per category
+class BooksInline(admin.TabularInline):
+        model = Book
+        extra = 3  # Add three more books per category
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = [
         'name',
         'slug',
-        'products_count',  # Added field to display product count
-       
+        'description',  # Added field to display description
+        'books_count',  # Added field to display book count
     ]
     list_filter = ['name']
-   
+    # readonly_fields = ['created', 'updated']  # Added to hide fields from edit
     ordering = ['name']
-    # Added field to search by description
-    search_fields = ['name',]
-    inlines = [ProductsInline]  # Added inline admin form for managing products
-    fields = ['name', 'slug',]  # Reordered fields
-    def products_count(self, obj):
-        return obj.products.count()
+    search_fields = ['name', 'description']  # Added field to search by description
+    inlines = [BooksInline]  # Added inline admin form for managing books
+    fields = ['name', 'slug', 'description']  # Reordered fields
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+    def books_count(self, obj):
+        return obj.books.count()
+
+    books_count.short_description = 'Number of Books'
+
+
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
     list_display = [
-        'name',
+        'first_name',
+        'last_name',
         'slug',
+        'bio',
+    ]
+    list_filter = ['first_name', 'last_name']  # Added filter for first and last names
+    
+
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    
+
+    list_display = [
+        'title',
+        'slug',
+        'author',
+        'ISBN',
         'price',
-        'category',
         'available',
         'created',
         'updated',
     ]
-    list_filter = ['category', 'available', 'created', 'updated']
-    list_editable = ['price', 'available']
+    list_filter = ['available', 'created', 'updated','author']
+    list_editable = ['price', 'available',]
     list_per_page = 10  # Set pagination to 10 items per page
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('title',)}
     readonly_fields = ['created', 'updated']
-    # Added custom actions
-    actions = ['mark_as_unavailable', 'restore_availability']
+    actions = ['mark_as_unavailable', 'restore_availability']  # Added custom actions
 
     def mark_as_unavailable(self, request, queryset):
         for obj in queryset.filter(available=True):
             obj.available = False
             obj.save()
-        self.message_user(
-            request, f'{queryset.count()} products marked as unavailable')
+        self.message_user(request, f'{queryset.count()} books marked as unavailable')
 
     def restore_availability(self, request, queryset):
         for obj in queryset.filter(available=False):
             obj.available = True
             obj.save()
-        self.message_user(
-            request, f'{queryset.count()} products restored to availability')
+        self.message_user(request, f'{queryset.count()} books restored to availability')
 
     mark_as_unavailable.short_description = 'Mark as unavailable'
     restore_availability.short_description = 'Restore availability'
+
+    
+
+
+
+
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = [
+#         'name',
+#         'slug',
+#         'price',
+#         'category',
+#         'available',
+#         'created',
+#         'updated',
+#     ]
+#     list_filter = ['category', 'available', 'created', 'updated']
+#     list_editable = ['price', 'available']
+#     list_per_page = 10  # Set pagination to 10 items per page
+#     prepopulated_fields = {'slug': ('name',)}
+#     readonly_fields = ['created', 'updated']
+#     # Added custom actions
+#     actions = ['mark_as_unavailable', 'restore_availability']
+
+#     def mark_as_unavailable(self, request, queryset):
+#         for obj in queryset.filter(available=True):
+#             obj.available = False
+#             obj.save()
+#         self.message_user(
+#             request, f'{queryset.count()} products marked as unavailable')
+
+#     def restore_availability(self, request, queryset):
+#         for obj in queryset.filter(available=False):
+#             obj.available = True
+#             obj.save()
+#         self.message_user(
+#             request, f'{queryset.count()} products restored to availability')
+
+#     mark_as_unavailable.short_description = 'Mark as unavailable'
+#     restore_availability.short_description = 'Restore availability'
 
     
 
