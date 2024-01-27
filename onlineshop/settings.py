f6fc4e72c7c6a4f54444cc124ff75bd95cae5462
aliqnbri <aliqnbri1998@gmail.com>
2024-01-27 16:11:07 +0330

    Feat: custom users created.

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CART_SESSION_ID = 'cart'
+
+AUTH_USER_MODEL = "user.CustomUser"
