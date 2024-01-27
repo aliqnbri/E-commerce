0d51635eee7a05e6ac1479e9aa97229e610afe5d
aliqnbri <aliqnbri1998@gmail.com>
2024-01-27 21:38:44 +0330

    Docs: requiremts updated.

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.conf import settings
 
 
 # Create your models here.
@@ -73,3 +74,16 @@ class Book(models.Model):
         return self.title
 
 
+class Review(models.Model):
+    """
+    A Django model representing a review for a specific book.
+    """
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])
+    comment = models.TextField(max_length=1000, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review for {self.book.title} by {self.user.username}"
--- user/models.py
+++ user/models.py
@@ -4,13 +4,15 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
-
-
+from django-phonenumber-field
+from phonenumber_field.modelfields import PhoneNumberField
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone_number = PhoneNumberField(unique=True)
     email = models.EmailField(_("email address"), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
+    
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
