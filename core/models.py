e7cabe444674e346a92e1a9ba2c04bcac55dc775
aliqnbri <aliqnbri1998@gmail.com>
2024-01-30 13:09:42 +0330

    Fix: fixed some tailwind problems

--- core/models.py
+++ core/models.py
@@ -13,3 +13,13 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+
+
+class ShopInfo(models.Model):
+    shop_name = models.CharField(max_length=255, verbose_name='Shop Name')
+    address = models.CharField(max_length=255, verbose_name='Address')
+    phone_number = models.CharField(max_length=12, verbose_name='Phone Number')
+    locations = models.TextField(verbose_name='Locations')
+    extra = models.TextField(verbose_name='Extra Info')
