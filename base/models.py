6841db460ddbbfe8d9a8bef068cd76156e86e21b
aliqnbri <aliqnbri1998@gmail.com>
2024-01-30 09:27:49 +0330

    Refactor: BaseModel created.

---
+++ base/models.py
@@ -0,0 +1,12 @@
+from django.db import  models
+
+import uuid 
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField (auto_now=True)
+
+
+    class Meta:
+        abstact  = True
