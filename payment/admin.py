7e864c45a92b4f12558c6539040e3e64a4f84e1b
aliqnbri <aliqnbri1998@gmail.com>
2024-01-31 10:09:08 +0330

    Dev: register payment and trasaction models.

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Payment, Transaction
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'order', 'amount', 'status')
+    list_filter = ('status',)
+    search_fields = ('customer__username', 'order__order_number')
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'payment', 'name', 'city', 'state')
+    list_filter = ('state',)
+    search_fields = ('customer__username', 'name', 'city')
