d01f1b110eef56daa180bd8e58945756d8bb1c79
aliqnbri <aliqnbri1998@gmail.com>
2024-02-06 00:08:06 +0330

    Feat: register templates created.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
      path('', include('django.contrib.auth.urls')),
      path('', views.dashboard, name='dashboard'),
+     path('register/', views.register, name= 'register')
 ]
