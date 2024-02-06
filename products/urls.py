e453a2f8504fd8f1fea808b7382417fb50945e2a
aliqnbri <aliqnbri1998@gmail.com>
2024-02-06 10:31:12 +0330

    Dev: products urls.py created.

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
 urlpatterns = [
-    # path('',views.index , name='index'),
+    path('',views.product_list, name= 'product_list'),
+    path('<slug:category_slug>/', views.product_list, name='products_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
